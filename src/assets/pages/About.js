5a26b44716260c3234ca830775a3c90315905497
Youpizo <loic.deruy@sfr.fr>
2025-05-02 17:17:30 +0200

    chantier banner

--- src/assets/pages/About.js
+++ src/assets/pages/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { HeaderBanner1, } from "../components/Banner/Banner";
 import About from "../components/About/About"
 
 const AboutPage = () => {
--- src/assets/pages/Installation.js
+++ src/assets/pages/Installation.js
@@ -1,7 +1,5 @@
 import React from "react";
 import InstallContent from "../components/InstallContent/InstallContent";
-import { HeaderBanner1 } from "../components/Banner/Banner";
-
 
 function InstallationPage () {
     return (
